1d618c049e2882c741183cfb9bde78d378845dfb
grinella <113719520+grinella@users.noreply.github.com>
2024-01-20 22:31:40 +0100

    Add files via upload

---
+++ MINISHELL/include/minishell.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grinella <grinella@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 20:04:07 by grinella          #+#    #+#             */
+/*   Updated: 2024/01/20 20:34:41 by grinella         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <stdio.h>
+# include <termios.h>
+# include <sys/ioctl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/stat.h>
+
+typedef struct	s_mini
+{
+	//variabile per token
+	variabile
+	variabile
+	variabile
+	variabile
+	variabile
+	variabile
+}				t_mini
+
+# endif
